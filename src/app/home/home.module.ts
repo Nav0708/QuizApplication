e50a8e729c52c231d92eb4dadaf9a9c409b2fdf6
Navami <navamibhat0708@gmail.com>
2023-11-23 15:18:43 -0800

    first commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,10 @@
+// home.module.ts
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule],
+})
+export class HomeModule {}
