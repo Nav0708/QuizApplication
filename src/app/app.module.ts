c9ed861d728c33bfc66226d85fa98051b9fa16d2
Navami <navamibhat0708@gmail.com>
2023-11-24 12:33:05 -0800

    adding pagenotFound and provider for routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InterviewsetupComponent } from './interviewsetup/interviewsetup.component';
-import { FormsModule } from '@angular/forms';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent,InterviewsetupComponent],
-  imports: [BrowserModule, DashboardModule, FormsModule],
+  declarations: [AppComponent, DashboardComponent,InterviewsetupComponent, PageNotFoundComponent],
+  imports: [FormsModule,ReactiveFormsModule,BrowserModule, DashboardModule],
   bootstrap: [AppComponent],
+  providers: [provideRouter(routes)]
 })
 export class AppModule {}
