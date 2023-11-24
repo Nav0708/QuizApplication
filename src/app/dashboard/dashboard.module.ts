a898a9bb2e709cba0c952af149702a14a38c5ebb
Navami <navamibhat0708@gmail.com>
2023-11-24 12:35:37 -0800

    adding formsmodule in dashboard modules for ngModule

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,11 @@
-// home.module.ts
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { InterviewsetupComponent } from '../interviewsetup/interviewsetup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DashboardComponent,InterviewsetupComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class DashboardModule {}
