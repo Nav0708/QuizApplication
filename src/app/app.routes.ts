dc44103915d8d9bacd1e5fa682698c3d08c5fa12
Navami <navamibhat0708@gmail.com>
2023-11-24 12:33:56 -0800

    adding paths for redirect and pagenotfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { InterviewsetupComponent } from './interviewsetup/interviewsetup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: 'app-interview-setup', component: InterviewsetupComponent},
+{path: 'app-dashboard', component: DashboardComponent},
+{ path: '',   redirectTo: '/app-dashboard', pathMatch: 'full' },
+{ path: '**', component: PageNotFoundComponent }];
+
