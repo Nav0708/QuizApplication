e74cbdd5ca4428ef1702531a3cb78217f3d72f54
Navami <navamibhat0708@gmail.com>
2023-11-25 00:02:35 -0800

    removed redundant routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InterviewsetupComponent } from './interviewsetup/interviewsetup.component';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
+import { AppRoutingModule, routes } from './app.routes';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { InterviewsimulatorComponent } from './interviewsimulator/interviewsimulator.component';
 
 
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent,InterviewsetupComponent, PageNotFoundComponent],
-  imports: [FormsModule,ReactiveFormsModule,BrowserModule, DashboardModule],
+  declarations: [AppComponent, DashboardComponent,InterviewsetupComponent, PageNotFoundComponent,InterviewsimulatorComponent],
+  imports: [FormsModule,ReactiveFormsModule,MatCardModule,BrowserModule, DashboardModule, AppRoutingModule],
   bootstrap: [AppComponent],
-  providers: [provideRouter(routes)]
+  
 })
 export class AppModule {}
