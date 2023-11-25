90f16adfe77f1c5f8d23482cbea47b39b18e0266
Navami <navamibhat0708@gmail.com>
2023-11-25 00:01:44 -0800

    indentation of components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InterviewsetupComponent } from './interviewsetup/interviewsetup.component';
+import { InterviewsimulatorComponent } from './interviewsimulator/interviewsimulator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,DashboardComponent,InterviewsetupComponent,RouterOutlet],
+  imports: [
+    RouterOutlet,
+    DashboardComponent,
+    InterviewsetupComponent,
+    RouterOutlet,
+    InterviewsimulatorComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
